4ab312ad303c5512f052739ab2aa4109645a7269
Elef Rosales <earq14@gmail.com>
2024-11-13 07:57:02 -0500

    feat: asiign default on port

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,7 +5,7 @@ import { get } from 'env-var';
 
 
 export const envs = {
-    PORT: get('PORT').required().asPortNumber(),
+    PORT: get('PORT').default(8080).required().asPortNumber(),
     DB_URL: get('DB_URL').required().asString(),
     DB_NAME: get('DB_NAME').required().asString(),
     JWT_SECRET: get('JWT_SECRET').required().asString(),
